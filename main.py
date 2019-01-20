48ff672735a55c6b3da505cb828fa72553ae9fd7
Ivan Radonov <39803187+ikar1234@users.noreply.github.com>
2019-01-20 15:22:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+from math import sqrt
 
 N = 85000  # choose accuracy, the bigger the number, the better the estimate, but the slower to compute
 x = np.random.rand(N)
 y = np.random.rand(N)
-pi_xy = [(x[i], y[i]) for i in range(N) if x[i] ** 2 + y[i] ** 2 <= 1]
+pi_xy = [(x[i], y[i]) for i in range(N) if sqrt(x[i] ** 2 + y[i] ** 2) <= 1]
 
 print(4 * len(pi_xy) / len(x)) # gives us an estmate of pi
 
