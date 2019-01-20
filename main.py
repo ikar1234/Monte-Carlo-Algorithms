862873ed34e8df5344937504fb858680f1c9c2b4
Ivan Radonov <39803187+ikar1234@users.noreply.github.com>
2019-01-20 14:41:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# print(sum(l)/len(l))
-N = 8500
+N = 85000  # choose accuracy, the bigger the number, the better the estimate, but the slower to compute
 x = np.random.rand(N)
 y = np.random.rand(N)
 pi_xy = [(x[i], y[i]) for i in range(N) if x[i] ** 2 + y[i] ** 2 <= 1]
+
+print(4 * len(pi_xy) / len(x)) # gives us an estmate of pi
+
+""" If you want to plot the points in the circle
 pi_x = [x[0] for x in pi_xy]
 pi_y = [x[1] for x in pi_xy]
 plt.scatter(pi_x, pi_y)
 plt.show()
-print(4 * len(pi_xy) / len(x))
+"""
