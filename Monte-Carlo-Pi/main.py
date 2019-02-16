f6b043225e6c9b39455916dec23d00bd24a5fa19
Ivan Radonov <39803187+ikar1234@users.noreply.github.com>
2019-02-16 18:03:19 +0100

    Update main.py

--- Monte-Carlo-Pi/main.py
+++ Monte-Carlo-Pi/main.py
@@ -25,7 +25,6 @@ def monte_carlo(acc):
 N = 10**7  # choose accuracy 
 a = np.array([[monte_carlo(N)[0],monte_carlo(N)[1]] for x in range(350)])
 print(sum(a[:,0])/len(a[:,0]))  # average run-time -> ~1.17642 with 350 and ~0.82731 with 3 tries
-print(sum(a[:,1])/len(a[:,1]))  # average estimation -> 3.141623922285716 with 350 tries
+print(sum(a[:,1])/len(a[:,1]))  # average estimation -> 3.1415935862857154 with 350 tries!
 print(sorted([(x,abs(x-pi)) for x in a[:,1]],key=lambda x: x[0])[0]) # finds the best estimation and the difference with pi
- # 
   
