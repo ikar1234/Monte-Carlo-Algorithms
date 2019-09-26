f163a3bf6dddf8597bde7f8faa29d8e234f76637
Ivan Radonov <39803187+ikar1234@users.noreply.github.com>
2019-09-26 22:26:27 +0200

    Update ASM.py

--- Simulated Annealing/ASM.py
+++ Simulated Annealing/ASM.py
@@ -1,10 +1,10 @@
 """
-Ukkonen's algorithm for approximate pattern matching takes space proportional to the length of the text and the pattern.
-Also it doesn't work with scores(?).
-I will try to design a Metropolis Hastings optimization algorithm for finding the best scoring hit.
+Ukkonen's algorithm for approximate pattern matching takes approximately O(p^2) space, where p is pattern length.
+Moreover, the original algorithm doesn't work with scores.
+I will try to design a Metropolis Hastings optimization algorithm, better known as Simulated Annealing, for finding
+the best scoring hit.
 """
 import numpy as np
-from matplotlib import pyplot as plt
 from Bio import pairwise2
 from collections import namedtuple
 
@@ -17,22 +17,24 @@ def mh(text: str, pattern: str, n: int = 100, temp=2.0, beta=0.9):
     t = len(text)
     p = len(pattern)
     # initial position
-    k = np.random.randint(low=0,high=t-p)
+    k = np.random.randint(low=0, high=t-p)
     init_score = pairwise2.align.globalxx(text[k:k+p], pattern)[0][2]
-    Max = namedtuple('Max', ['position', 'score'])
-    m = Max(k, init_score)
+    Match = namedtuple('Match', ['position', 'score'])
+    m = Match({k}, init_score)
     for _ in range(n):
         # generate candidate position
         k = np.random.randint(low=0, high=t - p)
         score = pairwise2.align.globalxx(text[k:k + p], pattern)[0][2]
         delta = init_score-score
-        a = np.random.random(1)
         # acceptance probability
         prob = np.exp(-delta/temp)
-        acc = a < prob
+        acc = np.random.random(1) < prob
         if delta < 0 or acc:
-            init_score = score
-            m = Max(k,init_score)
+            if init_score==score:
+                m[0].add(k)
+            else:
+                init_score = score
+                m = Match({k}, init_score)
             temp *= beta
 
     return m
